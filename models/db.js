4c407d48ef9240bea531e0279c2f1a7cc4a24240
drkmaster1412 <98210345+drkmaster1412@users.noreply.github.com>
2022-06-25 15:00:24 +0700

    Employees Crud

---
+++ models/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+// const url = "mongodb://localhost:27017/EmployeeDB";
+const url = "darkmaster1412:huyhoang022002@cluster0.nqtmz.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(url,{useNewUrlParser:true},(err) => {
+    if(!err){ console.log("MongoDB Connection Succeeded");}
+    else{
+        console.log("An Error Occured");
+    } 
+})
+
+require('./employee.model');
