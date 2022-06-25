118bd5d9bc71c55db590c5a3d6ab8d947589e3b8
drkmaster1412 <98210345+drkmaster1412@users.noreply.github.com>
2022-06-25 15:09:03 +0700

    db change

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 // const url = "mongodb://localhost:27017/EmployeeDB";
-const url = "darkmaster1412:huyhoang022002@cluster0.nqtmz.mongodb.net/?retryWrites=true&w=majority"
+const url = "mongodb+srv://darkmaster1412:huyhoang022002@cluster0.nqtmz.mongodb.net/test?retryWrites=true&w=majority"
 mongoose.connect(url,{useNewUrlParser:true},(err) => {
     if(!err){ console.log("MongoDB Connection Succeeded");}
     else{
